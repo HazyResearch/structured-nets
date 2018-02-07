e21e905a2e7233b21cddbbbef683b862096ca4a8
Anna <thomasat@stanford.edu>
2018-02-07 00:06:12 -0800

    add option to test on test set

--- dataset.py
+++ dataset.py
@@ -5,40 +5,54 @@ from sklearn.preprocessing import OneHotEncoder
 sys.path.insert(0, '../../../../')
 from utils import *
 
+supported_datasets = ['mnist', 'mnist_rot', 'mnist_bg_rot', 'mnist_rand_bg', 'mnist_noise', 'convex', 'rect', 
+	'rect_images']
+
 class Dataset:
-	def __init__(self, name, n, test_size=1000):
+	# true_test: if True, we test on test set. Otherwise, split training set into train/validation.
+	def __init__(self, name, n, test_size=1000, true_test=False):
 		self.name = name
 		self.mnist = None
 		self.true_transform = None
 		self.test_size = test_size
+		self.true_test = true_test
 		self.n = n
-		if self.name == 'mnist':
-			data_dir = '/tmp/tensorflow/mnist/input_data'
-			self.mnist = input_data.read_data_sets(data_dir, one_hot=True)
-			self.test_X = self.mnist.test.images
-			self.test_Y = self.mnist.test.labels
-		elif self.name == 'mnist_rot':
-			data_loc = '/dfs/scratch1/thomasat/datasets/mnist_rot/mnist_all_rotation_normalized_float_train_valid.amat'
-			self.load_data(data_loc)
-		elif self.name == 'mnist_bg_rot':
-			data_loc = '/dfs/scratch1/thomasat/datasets/mnist_bg_rot/mnist_all_background_images_rotation_normalized_train_valid.amat'
-			self.load_data(data_loc)
-		elif self.name == 'mnist_rand_bg':
-			data_loc = '/dfs/scratch1/thomasat/datasets/mnist_rand_bg/mnist_background_random_train.amat'
-			self.load_data(data_loc)
-		elif self.name.startswith('mnist_noise'):
-			idx = self.name[-1]
-			data_loc = '/dfs/scratch1/thomasat/datasets/mnist_noise/mnist_noise_variations_all_' + idx + '.amat'
-			self.load_data(data_loc)
-		elif self.name == 'convex':
-			data_loc = '/dfs/scratch1/thomasat/datasets/convex/convex_train.amat'
-			self.load_data(data_loc)
-		elif self.name == 'rect':
-			data_loc = '/dfs/scratch1/thomasat/datasets/rect/rectangles_train.amat'
-			self.load_data(data_loc)
-		elif self.name == 'rect_images':
-			data_loc = '/dfs/scratch1/thomasat/datasets/rect_images/rectangles_im_train.amat'
-			self.load_data(data_loc)
+		if self.name in supported_datasets:
+			if self.name == 'mnist':
+				data_dir = '/tmp/tensorflow/mnist/input_data'
+				self.mnist = input_data.read_data_sets(data_dir, one_hot=True)
+				self.test_X = self.mnist.test.images
+				self.test_Y = self.mnist.test.labels
+			elif self.name == 'mnist_rot':
+				train_data_loc = '/dfs/scratch1/thomasat/datasets/mnist_rot/mnist_all_rotation_normalized_float_train_valid.amat'
+				test_data_loc = '/dfs/scratch1/thomasat/datasets/mnist_rot/mnist_all_rotation_normalized_float_test.amat'
+				self.load_data(train_data_loc, test_data_loc)
+			elif self.name == 'mnist_bg_rot':
+				train_data_loc = '/dfs/scratch1/thomasat/datasets/mnist_bg_rot/mnist_all_background_images_rotation_normalized_train_valid.amat'
+				test_data_loc = '/dfs/scratch1/thomasat/datasets/mnist_bg_rot/mnist_all_background_images_rotation_normalized_test.amat'
+				self.load_data(train_data_loc, test_data_loc)
+			elif self.name == 'mnist_rand_bg':
+				train_data_loc = '/dfs/scratch1/thomasat/datasets/mnist_rand_bg/mnist_background_random_train.amat'
+				test_data_loc = '/dfs/scratch1/thomasat/datasets/mnist_rand_bg/mnist_background_random_test.amat'
+				self.load_data(train_data_loc, test_data_loc)
+			elif self.name.startswith('mnist_noise'):
+				idx = self.name[-1]
+				train_data_loc = '/dfs/scratch1/thomasat/datasets/mnist_noise/mnist_noise_variations_all_' + idx + '.amat'
+				test_data_loc = '/dfs/scratch1/thomasat/datasets/mnist_noise/mnist_noise_variations_all_' + idx + '.amat'
+				self.test_size = 2000 # As specified in http://www.iro.umontreal.ca/~lisa/twiki/bin/view.cgi/Public/DeepVsShallowComparisonICML2007#Downloadable_datasets
+				self.load_data(train_data_loc, test_data_loc)
+			elif self.name == 'convex':
+				train_data_loc = '/dfs/scratch1/thomasat/datasets/convex/convex_train.amat'
+				test_data_loc = '/dfs/scratch1/thomasat/datasets/convex/50k/convex_test.amat'
+				self.load_data(train_data_loc, test_data_loc)
+			elif self.name == 'rect':
+				train_data_loc = '/dfs/scratch1/thomasat/datasets/rect/rectangles_train.amat'
+				test_data_loc = '/dfs/scratch1/thomasat/datasets/rect/rectangles_test.amat'
+				self.load_data(train_data_loc, test_data_loc)
+			elif self.name == 'rect_images':
+				train_data_loc = '/dfs/scratch1/thomasat/datasets/rect_images/rectangles_im_train.amat'
+				test_data_loc = '/dfs/scratch1/thomasat/datasets/rect_images/rectangles_im_test.amat'
+				self.load_data(train_data_loc, test_data_loc)
 		elif self.name.startswith('true'):
 			self.true_transform = gen_matrix(n, self.name.split("true_",1)[1] )
 			test_X, test_Y = gen_batch(self.true_transform, self.test_size)
@@ -56,20 +70,38 @@ class Dataset:
 		else:
 			return self.n
 
-	def load_data(self, data_loc):
-		data = np.genfromtxt(data_loc)
-		# Last self.test_size examples are for validation
-		# Last column is true class
+	def load_data(self, train_loc, test_loc):
+		if train_loc == test_loc:
+			assert not self.true_test or self.name.startswith('mnist_noise')
 
-		self.train_X = data[:-self.test_size, :-1]
-		self.train_Y = np.expand_dims(data[:-self.test_size, -1], 1)
-		self.test_X = data[-self.test_size:, :-1]
-		self.test_Y = np.expand_dims(data[-self.test_size:, -1], 1)
+		if self.true_test and train_loc != test_loc:
+			train_data = np.genfromtxt(train_loc)
+			test_data = np.genfromtxt(test_loc)
 
-		# Y must be one-hot
-		enc = OneHotEncoder()
-		self.train_Y = enc.fit_transform(self.train_Y).todense()
-		self.test_Y = enc.fit_transform(self.test_Y).todense()
+			self.train_X = train_data[:, :-1]
+			self.train_Y = np.expand_dims(train_data[:, -1], 1)
+			self.test_X = test_data[:, :-1]
+			self.test_Y = np.expand_dims(test_data[:, -1], 1)
+
+			# Y must be one-hot
+			enc = OneHotEncoder()
+			self.train_Y = enc.fit_transform(self.train_Y).todense()
+			self.test_Y = enc.fit_transform(self.test_Y).todense()
+
+		else: # Use train_loc only
+			data = np.genfromtxt(train_loc)
+			# Last self.test_size examples are for validation
+			# Last column is true class
+
+			self.train_X = data[:-self.test_size, :-1]
+			self.train_Y = np.expand_dims(data[:-self.test_size, -1], 1)
+			self.test_X = data[-self.test_size:, :-1]
+			self.test_Y = np.expand_dims(data[-self.test_size:, -1], 1)
+
+			# Y must be one-hot
+			enc = OneHotEncoder()
+			self.train_Y = enc.fit_transform(self.train_Y).todense()
+			self.test_Y = enc.fit_transform(self.test_Y).todense()
 
 		print self.train_X.shape, self.train_Y.shape, self.test_X.shape, self.test_Y.shape
 
