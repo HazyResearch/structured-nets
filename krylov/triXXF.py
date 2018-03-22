51fbe266dde5d498646d16bfac25622edb1e1b66
Tri Dao <tridpq@gmail.com>
2018-03-22 00:20:28 -0700

    Speed up Krylov multiplication by 30%

--- krylov/triXXF.py
+++ krylov/triXXF.py
@@ -199,6 +199,60 @@ def resolvent_bilinear_flattened_(subd, v, u, m, d, S):
 
     return (T_00, T_01, T_10, T_11)
 
+# this is specialized to subdiagonal for now
+def resolvent_bilinear_flattened_batched_(subd, v, u, m, d, S):
+    # pass at depth d computes 4 arrays:
+    # each array is length n, indexed by x_{m-1}, ..., x_{m-d}, y_{m-d-1}, ..., y_0
+    # for convenience, store as x_{m-d}, ..., x_{m-1}, y_{m-d-1}, ..., y_0
+
+    assert d < m # assume leaf pass done in main function
+
+    S_00, S_01, S_10, S_11 = S # answers to previous layer: indexed by x_{m-d-1}, x_{m-d}, ..., x_{m-1}, y_{m-d-2}, ..., y_0
+    # these are the same as the S0[0,0],S1[0,0] above
+    assert S_00.shape == (1<<(d+1), 1<<(m-d-1))
+    n1, n2 = 1<<d, 1<<(m-d)
+
+    # TODO: time the following: hcat a big dense matrix with a 0 matrix, vs. np.zeros followed by assignment
+    # %timeit np.hstack((x, np.zeros((32,32))))   : 9.16 µs ± 221 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
+    # %timeit y = np.zeros((32,64)); y[:,:32] = x : 3.63 µs ± 573 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
+    # S0_00, S0_01, S0_00, S0_01, S1_00, S1_01, S1_00, S1_01 = np.zeros((8,n))
+    # S0_00[:n/2], S1_00[:n/2] = S_00[:n/2], S_00[n/2:] # alternative: numpy.split(S, 2)
+    # S0_01[:n/2], S1_01[:n/2] = S_01[:n/2], S_01[n/2:]
+    # S0_10[:n/2], S1_10[:n/2] = S_10[:n/2], S_10[n/2:]
+    # S0_11[:n/2], S1_11[:n/2] = S_11[:n/2], S_11[n/2:]
+    S_storage = np.zeros((4, n1, n2))
+    S0_10, S0_11, S1_01, S1_11 = S_storage
+    S0_10[:,:n2//2] = S_10[:n1,:]
+    S1_01[:,:n2//2] = S_01[n1:,:]
+    S0_11[:,:n2//2] = S_11[:n1,:]
+    S1_11[:,:n2//2] = S_11[n1:,:]
+
+    # polynomial multiplications
+    # S0_10_ = np.fft.rfft(S0_10)
+    # S0_11_ = np.fft.rfft(S0_11)
+    # S1_01_ = np.fft.rfft(S1_01)
+    # S1_11_ = np.fft.rfft(S1_11)
+    storage_fft = np.fft.rfft(S_storage)
+    S0_10_, S0_11_, S1_01_, S1_11_ = storage_fft
+
+    # subproblem for branch x_{m-d}, ..., x_{m-1} is A[\overline{x_{m-1}...x_{m-d}} + 2^{m-d-1}]
+    A_subd = subd[n1:n1*2, np.newaxis]
+    # T_00 = A_subd * np.fft.irfft(S1_01_ * S0_10_)
+    # T_01 = A_subd * np.fft.irfft(S1_01_ * S0_11_)
+    # T_10 = A_subd * np.fft.irfft(S1_11_ * S0_10_)
+    # T_11 = A_subd * np.fft.irfft(S1_11_ * S0_11_)
+    T_storage = np.stack((S1_01_ * S0_10_, S1_01_ * S0_11_, S1_11_ * S0_10_, S1_11_ * S0_11_))
+    T_storage = A_subd * np.fft.irfft(T_storage)
+    T_00, T_01, T_10, T_11 = T_storage
+
+    # polynomial additions
+    T_00[:,n2//2:] += S_00[:n1,:]
+    T_00[:,n2//2:] += S_00[n1:,:]
+    T_01[:,n2//2:] += S_01[:n1,:]
+    T_10[:,n2//2:] += S_10[n1:,:]
+
+    return (T_00, T_01, T_10, T_11)
+
 
 def bitreversal_fat(x, n, m):
     """ Compute the bit reversal permutation """
@@ -243,6 +297,27 @@ def resolvent_bilinear_flattened(A, v, u, n, m):
     return S[0].squeeze()[::-1]
 
 
+def resolvent_bilinear_flattened_batched(A, v, u, n, m):
+    assert n == 1<<m # power of 2 for now
+
+    # assume A is subdiagonal for now
+    subd = np.concatenate(([0],np.diagonal(A, -1)))
+    subd = bitreversal(subd, n, m)
+
+    # reshape u,v to be indexed consistently with the above
+    # i.e. bit flip their indices
+    u_bf = bitreversal(u, n, m).reshape((n,1)) # tri says use [:,np.newaxis]
+    v_bf = bitreversal(v, n, m).reshape((n,1))
+
+    S = (u_bf*v_bf, u_bf, v_bf, np.ones((n,1)))
+
+    for d in range(m-1,-1,-1):
+        S = resolvent_bilinear_flattened_batched_(subd, v, u, m, d, S)
+
+    # print(S[0], S[1], S[2], S[3])
+    # return np.flip(S[0], axis=-1)
+    return S[0].squeeze()[::-1]
+
 
 def krylov_mult(A, v, u, m):
     """
@@ -275,6 +350,18 @@ def krylov_mult_slow(A, v, u, m):
         cols.append(Amult(d, subd, cols[-1]))
     K = np.stack(cols, axis=1)
     return K.T @ u
+
+def krylov_mult_slow_allocated(A, v, u, m):
+    n = v.shape[0]
+    assert A.shape == (n,n)
+    d = np.diagonal(A, 0)
+    subd = np.diagonal(A, -1)
+    # Allocate memory at once to K
+    K_T = np.empty((m, n))
+    K_T[0] = v
+    for i in range(1,m):
+        K_T[i] = Amult(d, subd, K_T[i-1])
+    return K_T @ u
 
 def krylov_construct(A, v, m):
     n = v.shape[0]
@@ -326,9 +413,11 @@ A = np.diag(np.random.random(n-1), -1)
 u = np.random.random(n)
 v = np.random.random(n)
 k1 = krylov_mult_slow(A,v,u,n)
+k1_allocated = krylov_mult_slow_allocated(A,v,u,n)
 k11 = krylov_mult_slow_faster(A,v,u,n)
 k2 = krylov_mult(A,v,u,n)
 k3 = resolvent_bilinear_flattened(A, v, u, n, m)
+k3b = resolvent_bilinear_flattened_batched(A, v, u, n, m)
 print(np.max(np.abs(k1-k11)))
 print(np.max(np.abs(k1-k2)))
 print(np.max(np.abs(k1-k3)))
