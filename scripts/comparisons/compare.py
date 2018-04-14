8b47c622f6a862eee2bc1d9a6d849fe7213aea1a
thomasat <thomasat@stanford.edu>
2018-04-14 15:37:55 -0700

    modifying command line params

--- scripts/comparisons/compare.py
+++ scripts/comparisons/compare.py
@@ -13,7 +13,7 @@ import argparse
 
 # Available datasets: norb, cifar10, smallnorb, mnist, mnist_noise_variation_*, mnist_rand_bg, mnist_bg_rot, convex, rect, rect_images
 # Example command: 
-# python compare.py --name=test --methods=tridiagonal_corner,toeplitz-like --dataset=true_toeplitz --result_dir=2_25_18 --r=1 --lr=1e-3 --decay_rate=1.0 --mom=0.9 --test=0 --layer_size=50 --transform=none --torch=1 --model=Attention
+# python compare.py --name=test --methods=tridiagonal_corner,toeplitz-like --dataset=true_toeplitz --result_dir=2_25_18 --r=1 --lr=1e-3 --decay_rate=1.0 --decay_steps=0.1 --mom=0.9 --steps=50000 --batch_size=1024 --test=0 --layer_size=50 --transform=none --torch=1 --model=Attention
 
 # Command line params
 parser = argparse.ArgumentParser()
@@ -24,21 +24,21 @@ parser.add_argument("--result_dir") # Where to save results
 parser.add_argument("--r", type=int) # Rank / displacement rank
 parser.add_argument('--lr', type=float) # Learning rate
 parser.add_argument('--decay_rate', type=float) # Decay of learning rate
+parser.add_argument('--decay_freq', type=float) # Decay steps
 parser.add_argument('--mom', type=float) # Momentum
+parser.add_argument('--steps', type=int) # Steps
+parser.add_argument('--batch_size', type=int) # Batch size
 parser.add_argument('--test', type=int) # Test on test set
 parser.add_argument('--layer_size', type=int) # Size of hidden layer
 parser.add_argument('--transform') # Any transform of dataset, e.g. grayscale
-parser.add_argument('--torch') # Pytorch or TF
+parser.add_argument('--torch', type=int) # Pytorch or TF
 parser.add_argument('--model') # Which model, e.g. CNN, MLP, RNN
 args = parser.parse_args()
 
 # Fixed params
 num_layers = 1
-out_dir = '../../results'#'/dfs/scratch1/thomasat/'
+out_dir = '/dfs/scratch1/thomasat/'
 loss = 'cross_entropy'
-steps = 50000
-decay_freq = 0.1
-batch_size = 512
 test_size = 1000
 train_size = 10000
 verbose = False
@@ -52,14 +52,14 @@ test_freq = 100
 learn_corner = True
 learn_diagonal = False
 stochastic_train = False
-checkpoint_freq = 100000
+checkpoint_freq = 1000
 num_conv_layers = 2
 n_trials = 3
 log_path = os.path.join(out_dir, 'tensorboard', args.result_dir)
 results_dir = os.path.join(out_dir, 'results', args.result_dir) 
 checkpoint_path = os.path.join(out_dir, 'checkpoints', args.result_dir)
 
-dataset = Dataset(args.dataset, args.layer_size, steps, args.transform, 
+dataset = Dataset(args.dataset, args.layer_size, args.steps, args.transform, 
 	stochastic_train, test_size, train_size, args.test)
 n_diag_learned = dataset.input_size - 1
 commit_id = get_commit_id()
@@ -70,10 +70,10 @@ print('Testing methods: ', methods)
 for method in methods:
 	params = ModelParams(args.dataset, args.transform, args.test, log_path, 
 			dataset.input_size, args.layer_size, dataset.out_size(), num_layers, 
-			loss, args.r, steps, batch_size, args.lr, args.mom, init_type, 
+			loss, args.r, args.steps, args.batch_size, args.lr, args.mom, init_type, 
 			method, learn_corner, n_diag_learned, init_stddev, fix_G, 
 			check_disp, checkpoint_freq, checkpoint_path, test_freq, verbose, 
-			args.decay_rate, decay_freq, learn_diagonal, fix_A_identity, 
+			args.decay_rate, args.decay_freq, learn_diagonal, fix_A_identity, 
 			stochastic_train, flip_K_B, num_conv_layers, args.torch, args.model)
 
 	# Save params + git commit ID
@@ -94,8 +94,8 @@ for method in methods:
 		tf.reset_default_graph()
 
 		out_loc = os.path.join(this_results_dir, this_iter_name)
-		pkl.dump(losses, open(out_loc + '_losses.p', 'wb'))
-		pkl.dump(accuracies, open(out_loc + '_accuracies.p', 'wb'))
+		pkl.dump(losses, open(out_loc + '_losses.p', 'wb'), protocol=2)
+		pkl.dump(accuracies, open(out_loc + '_accuracies.p', 'wb'), protocol=2)
 
 		print('Saved losses and accuracies for ' + method + ' to: ' + out_loc)
 
