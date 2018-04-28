c5278b6418e9c6b1954468814283ab49e5842ab9
thomasat <thomasat@stanford.edu>
2018-04-28 15:19:38 -0700

    adding data_utils for preprocessing

---
+++ scripts/other/data_utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def normalize_data(data):
+    mean = np.mean(data,axis=0)
+    std = np.std(data,axis=0)
+    return apply_normalization(data,mean,std), mean, std
+
+def apply_normalization(data, mean, std):
+    normalized = (data-mean)/std
+    print('mean, std: ', np.mean(normalized,axis=0), np.std(normalized,axis=0))
+    return normalized
+
+def standardize(data, max_val=255.0):
+    scaled = data/max_val
+    scaled = (scaled - 0.5)/0.5
+    print('min, max: ', np.min(scaled), np.max(scaled))
+    return scaled
+
