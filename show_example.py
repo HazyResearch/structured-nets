5e72478600df01f0d099cb14a3fc7fffacb83910
Albert Gu <albertgu@stanford.edu>
2018-05-25 10:16:48 -0700

    code for showing digits from norb and bg-rot

--- show_example.py
+++ show_example.py
@@ -3,13 +3,31 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-idx = 1
-data_loc = '/dfs/scratch1/thomasat/datasets/mnist_noise/mnist_noise_variations_all_' + str(idx) + '.amat'
+import pickle as pkl
 
-np.random.seed(0)
-data = np.genfromtxt(data_loc)
+# MNIST variants
+# idx = 1
+# # data_loc = '/dfs/scratch1/thomasat/datasets/mnist_noise/mnist_noise_variations_all_' + str(idx) + '.amat'
+# data_loc = '/dfs/scratch1/thomasat/datasets/mnist_bg_rot/mnist_all_background_images_rotation_normalized_train_valid.amat'
+# data = np.genfromtxt(data_loc)
+# save_loc = 'mnist_bg_rot_digits.png'
+
+# np.random.seed(1)
+# samples = np.random.choice(data.shape[0], 4)
+# X = data[samples, :-1].reshape((-1, 28, 28))
+
+
+# NORB
+data_loc = '/dfs/scratch1/thomasat/datasets/norb_full/processed_py2_train_28.pkl'
+data = pkl.load(open(data_loc, 'rb'))
+data = data['X']
+
+save_loc = 'norb_digits.png'
+# self.train_Y = data['Y']
+
+np.random.seed(1)
 samples = np.random.choice(data.shape[0], 4)
-X = data[samples, :-1].reshape((-1, 28, 28))
+X = data[samples, :].reshape((-1, 28, 28))
 
 fig = plt.figure(figsize=(8,8))
 plt.subplot(2,2,1)
@@ -25,5 +43,9 @@ plt.subplot(2,2,4)
 plt.axis('off')
 plt.imshow(X[3,:], cmap='gray', interpolation='nearest')
 fig.subplots_adjust(wspace=0,hspace=0)
-plt.savefig('mnist_noise_digits.png', bbox_inches='tight')
+plt.savefig(save_loc, bbox_inches='tight')
 plt.close()
+
+
+
+
