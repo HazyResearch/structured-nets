2bb0a51c4b8b95f3ef06f76c0a78ed0af85a394f
Albert Gu <albertgu@stanford.edu>
2018-05-31 19:25:42 -0700

    move out ModelParams.save()

--- scripts/comparisons/compare.py
+++ scripts/comparisons/compare.py
@@ -8,6 +8,7 @@ sys.path.insert(0, '../../')
 import argparse
 import threading
 import logging
+import pprint
 import numpy as np
 import torch
 
@@ -20,6 +21,7 @@ from dataset import DatasetLoaders
 from dataset_copy import Dataset
 from torch_utils import get_loss
 from nets import construct_net
+from optimize_torch import optimize_torch
 
 def get_commit_id():
   return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
@@ -52,11 +54,11 @@ def compare_old(args, method, rank, lr, decay_rate, mom, train_frac, steps, trai
             tie_operators_same_layer, tie_layers_A_A, tie_layers_A_B, train_frac, bias)
 
     # Save params + git commit ID
-    this_id = args.name + '_' + method_map[method] + '_r' + str(rank) + '_lr' + str(lr) + '_dr' + str(decay_rate) + '_mom' + str(mom) + '_bs' + str(args.batch_size) + '_tf' + str(train_frac) + '_steps' + str(steps)
-    this_results_dir = params.save(results_dir, this_id, commit_id, command)
+    run_id = args.name + '_' + method_map[method] + '_r' + str(rank) + '_lr' + str(lr) + '_dr' + str(decay_rate) + '_mom' + str(mom) + '_bs' + str(args.batch_size) + '_tf' + str(train_frac) + '_steps' + str(steps)
+    this_results_dir = params.save(results_dir, run_id, commit_id, command)
 
     for test_iter in range(args.trials):
-        this_iter_name = this_id + '_' + str(test_iter)
+        this_iter_name = run_id + '_' + str(test_iter)
         params.log_path = os.path.join(log_path, this_iter_name)
         params.checkpoint_path = os.path.join(checkpoint_path, this_iter_name)
         params.vis_path = os.path.join(vis_path, this_iter_name)
@@ -96,7 +98,7 @@ parser.add_argument('--decay_freq', type=float) # Decay steps
 parser.add_argument('--mom', nargs='+', type=float, default=[0.9]) # Momentums
 parser.add_argument('--steps', type=int) # Steps
 parser.add_argument('--batch_size', type=int) # Batch size
-parser.add_argument('--test', type=int, default=1) # Test on test set
+parser.add_argument('--test', action='store_true') # Test on test set
 parser.add_argument('--layer_size', type=int) # Size of hidden layer
 parser.add_argument('--transform', default='none') # Any transform of dataset, e.g. grayscale
 parser.add_argument('--model') # Which model, e.g. CNN, MLP, RNN
@@ -159,8 +161,8 @@ tie_layers_A_A = False
 tie_layers_A_B = False
 
 
-commit_id = get_commit_id()
-command = ' '.join(sys.argv)
+# commit_id = get_commit_id()
+# command = ' '.join(sys.argv)
 
 # logging.debug('Testing methods: ' + str(args.method))
 # logging.debug('Testing ranks: ' + str(args.r))
@@ -203,7 +205,32 @@ def vae(args):
 
 #### REFACTORING MAIN CODE PATH
 
-def compare(args, dataset, method, rank, lr, decay_rate, mom, train_frac, steps, log_path, results_dir, checkpoint_path, vis_path):
+def save_args(args, results_dir):
+    commit_id = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
+    command = ' '.join(sys.argv)
+    param_str = str(commit_id) + '\n' + command + '\n' + pprint.pformat(vars(args))
+    print(param_str)
+
+    # Make new dir with timestamp
+    # run_results_dir = os.path.join(results_dir, run_name + '_' + str(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")))
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+
+    # Save the parameters in readable form
+    text_file = open(os.path.join(results_dir, 'params.txt'), "w")
+    text_file.write(param_str)
+    text_file.close()
+
+    # Save the Namespace object
+    pkl.dump(args, open(os.path.join(results_dir, 'params.p'), "wb"))
+
+    # return results_dir
+
+
+def compare(args, method, rank, lr, decay_rate, mom, train_frac, steps, log_path, results_dir, checkpoint_path, vis_path):
+
+    dataset = DatasetLoaders(args.dataset, args.transform, train_frac, None, args.batch_size)
+
     params = ModelParams(args.dataset, args.transform, args.test, log_path,
             args.layer_size, args.layer_size, dataset.out_size, num_layers,
             loss, rank, steps, args.batch_size, lr, mom, init_type,
@@ -216,39 +243,47 @@ def compare(args, dataset, method, rank, lr, decay_rate, mom, train_frac, steps,
             tie_operators_same_layer, tie_layers_A_A, tie_layers_A_B, train_frac, bias)
 
     # Save params + git commit ID
-    this_id = args.name + '_' + method_map[method] + '_r' + str(rank) + '_lr' + str(lr) + '_dr' + str(decay_rate) + '_mom' + str(mom) + '_bs' + str(args.batch_size) + '_tf' + str(train_frac) + '_steps' + str(steps)
-    this_results_dir = params.save(results_dir, this_id, commit_id, command)
+    run_name = args.name \
+               + '_' + method_map[method] \
+               + '_r' + str(rank) \
+               + '_lr' + str(lr) \
+               + '_dr' + str(decay_rate) \
+               + '_mom' + str(mom) \
+               + '_bs' + str(args.batch_size) \
+               + '_tf' + str(train_frac) \
+               + '_steps' + str(steps)
+    # run_results_dir = params.save(results_dir, run_name, commit_id, command)
+    results_dir = os.path.join(results_dir, run_name + '_' + str(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")))
+    save_args(args, results_dir)
 
-    for test_iter in range(args.trials):
-        this_iter_name = this_id + '_' + str(test_iter)
-        log_path = os.path.join(log_path, this_iter_name)
-        checkpoint_path = os.path.join(checkpoint_path, this_iter_name)
-        params.vis_path = os.path.join(vis_path, this_iter_name)
-        params.result_path = os.path.join(this_results_dir,this_iter_name)
+    for trial_iter in range(args.trials):
+        run_iter_name = run_name + '_' + str(trial_iter)
+        log_path = os.path.join(log_path, run_iter_name)
+        checkpoint_path = os.path.join(checkpoint_path, run_iter_name)
+        # vis_path = os.path.join(vis_path, run_iter_name)
+        result_path = os.path.join(results_dir,run_iter_name)
 
-        logging.debug('Tensorboard log path: ' + params.log_path)
-        logging.debug('Tensorboard checkpoint path: ' + params.checkpoint_path)
-        logging.debug('Tensorboard vis path: ' + params.vis_path)
-        logging.debug('Results dir: ' + params.result_path)
+        logging.debug('Tensorboard log path: ' + log_path)
+        logging.debug('Tensorboard checkpoint path: ' + checkpoint_path)
+        # logging.debug('Tensorboard vis path: ' + vis_path)
+        logging.debug('Results directory: ' + result_path)
 
-        if not os.path.exists(params.checkpoint_path):
-            os.makedirs(params.checkpoint_path)
+        if not os.path.exists(checkpoint_path):
+            os.makedirs(checkpoint_path)
+        # if not os.path.exists(vis_path):
+        #     os.makedirs(vis_path)
 
 
-        if not os.path.exists(params.vis_path):
-            os.makedirs(params.vis_path)
-
-        from optimize_torch import optimize_torch
         loss_name_fn = get_loss(params)
         net = construct_net(params)
-        optimizer = torch.optim.SGD(net.parameters(), lr=params.lr, momentum=params.mom)
-        losses, accuracies = optimize_torch(dataset, net, optimizer, log_path, checkpoint_path, params.test, loss_name_fn)
+        optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=mom)
+        losses, accuracies = optimize_torch(dataset, net, optimizer, log_path, checkpoint_path, args.test, loss_name_fn)
         # tf.reset_default_graph()
 
-        pkl.dump(losses, open(params.result_path + '_losses.p', 'wb'), protocol=2)
-        pkl.dump(accuracies, open(params.result_path + '_accuracies.p', 'wb'), protocol=2)
+        pkl.dump(losses, open(result_path + '_losses.p', 'wb'), protocol=2)
+        pkl.dump(accuracies, open(result_path + '_accuracies.p', 'wb'), protocol=2)
 
-        logging.debug('Saved losses and accuracies for ' + method + ' to: ' + params.result_path)
+        logging.debug('Saved losses and accuracies for ' + method + ' to: ' + result_path)
 
 
 
@@ -257,6 +292,9 @@ def compare(args, dataset, method, rank, lr, decay_rate, mom, train_frac, steps,
 # code should look like: for dataset: for model: for optimizer: construct training params (paths); train
 # some parts can be shared (e.g. paths)
 def run(args):
+    # pprint.pprint(args.__dict__)
+    # from optimize_torch import optimize_torch
+
     log_path = os.path.join(out_dir, 'tensorboard', args.result_dir)
     results_dir = os.path.join(out_dir, 'results', args.result_dir)
     checkpoint_path = os.path.join(out_dir, 'checkpoints', args.result_dir)
@@ -269,11 +307,9 @@ def run(args):
         # this_steps = int(train_frac*args.steps)
         this_steps = args.steps
         # dispatch dataset and training function based on task
-        dataset = DatasetLoaders(args.dataset, args.transform, train_frac, None, args.batch_size)
         # from optimize_torch import optimize_torch
         # training_fn = optimize_torch
 
-        n_diag_learned = 0
         for rank in args.r:
             for lr in args.lr:
                 for decay_rate in args.decay_rate:
@@ -282,7 +318,7 @@ def run(args):
                         #     logging.debug('Starting thread')
                         #     threading.Thread(target=compare,args=(args, args.method, rank, lr, decay_rate, mom, train_frac, this_steps, training_fn),).start()
                         # else:
-                        compare(args, dataset, args.method, rank, lr, decay_rate, mom, train_frac,this_steps, log_path, results_dir, checkpoint_path, vis_path)
+                        compare(args, args.method, rank, lr, decay_rate, mom, train_frac,this_steps, log_path, results_dir, checkpoint_path, vis_path)
 
 
 
