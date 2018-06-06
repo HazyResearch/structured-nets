6011a5d1fc4276d7fcfec72a839d8696b9b91c65
Albert Gu <albertgu@stanford.edu>
2018-06-06 09:49:32 -0700

    clean up compare

--- scripts/comparisons/compare.py
+++ scripts/comparisons/compare.py
@@ -105,7 +105,7 @@ parser.add_argument('--restore', type=int, default=0) # Whether to restore from 
 # parser.add_argument("--r", nargs='+', type=int, default=[0]) # Rank / displacement ranks
 parser.add_argument('--parallel') #
 parser.add_argument('--trials', type=int, default=1) #
-parser.add_argument('--batch_size', type=int) # Batch size
+parser.add_argument('--batch_size', type=int, default=50) # Batch size
 parser.add_argument("--epochs", type=int, default=1)
 parser.add_argument('--lr', nargs='+', type=float, default=[1e-3]) # Learning rates
 parser.add_argument('--decay_rate', type=float, nargs='+', default=[1.0]) # Decay rates of learning rate
@@ -221,7 +221,6 @@ def save_args(args, results_dir):
     print(param_str)
 
     # Make new dir with timestamp
-    # run_results_dir = os.path.join(results_dir, run_name + '_' + str(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")))
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
 
@@ -233,103 +232,38 @@ def save_args(args, results_dir):
     # Save the Namespace object
     pkl.dump(args, open(os.path.join(results_dir, 'params.p'), "wb"))
 
-    # return results_dir
 
 
-def compare(args, method, lr, decay_rate, mom, train_frac, log_path, results_dir, checkpoint_path, net):
-
-    dataset = DatasetLoaders(args.dataset, args.transform, train_frac, None, args.batch_size)
-
-    # Save params + git commit ID
-               # + '_' + method_map[method] \
-               # + '_r' + str(rank) \
-    run_name = args.name \
-               + '_lr' + str(lr) \
-               + '_dr' + str(decay_rate) \
-               + '_mom' + str(mom) \
-               + '_bs' + str(args.batch_size) \
-               + '_tf' + str(train_frac) \
-               # + '_steps' + str(steps)
-    results_dir = os.path.join(results_dir, run_name + '_' + str(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")))
-    save_args(args, results_dir)
-
-    for trial_iter in range(args.trials):
-        run_iter_name = run_name + '_' + str(trial_iter)
-        log_path = os.path.join(log_path, run_iter_name)
-        checkpoint_path = os.path.join(checkpoint_path, run_iter_name)
-        # vis_path = os.path.join(vis_path, run_iter_name)
-        result_path = os.path.join(results_dir,run_iter_name)
-
-        logging.debug('Tensorboard log path: ' + log_path)
-        logging.debug('Tensorboard checkpoint path: ' + checkpoint_path)
-        # logging.debug('Tensorboard vis path: ' + vis_path)
-        logging.debug('Results directory: ' + result_path)
-
-        os.makedirs(checkpoint_path, exist_ok=True)
-        # if not os.path.exists(checkpoint_path):
-        #     os.makedirs(checkpoint_path)
-        # if not os.path.exists(vis_path):
-        #     os.makedirs(vis_path)
-
-
-        # loss_name_fn = get_loss(params)
-        # net = construct_net(params)
-        optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=mom)
-        losses, accuracies = optimize_torch(dataset, net, optimizer, args.epochs, log_path, checkpoint_path, args.test)
-        # tf.reset_default_graph()
-
-        pkl.dump(losses, open(result_path + '_losses.p', 'wb'), protocol=2)
-        pkl.dump(accuracies, open(result_path + '_accuracies.p', 'wb'), protocol=2)
-
-        logging.debug('Saved losses and accuracies for ' + method + ' to: ' + result_path)
-
-
-
-# TODO: separate into several functions, model() creates model for some subset of params, optimizer() creates optimizer for some subset of params, train(), dataset()
-# have sample() to do something special in the sample complexity case, vae(), etc.
-# code should look like: for dataset: for model: for optimizer: construct training params (paths); train
-# some parts can be shared (e.g. paths)
 def mlp(args):
-    # pprint.pprint(args.__dict__)
-    # from optimize_torch import optimize_torch
-
-    # log_path = os.path.join(out_dir, 'tensorboard', args.result_dir)
-    # results_dir = os.path.join(out_dir, 'results', args.result_dir)
-    # checkpoint_path = os.path.join(out_dir, 'checkpoints', args.result_dir)
-    # vis_path = os.path.join(out_dir, 'vis', args.result_dir)
-
-
-    # TODO use itertools.product to do this
-    train_frac = None
-    for lr, decay_rate, mom in itertools.product(args.lr, args.decay_rate, args.mom):
+    for train_frac in args.train_frac:
         dataset = DatasetLoaders(args.dataset, args.transform, train_frac, None, args.batch_size)
+        for lr, decay_rate, mom in itertools.product(args.lr, args.decay_rate, args.mom):
 
-        run_name = args.name \
-                + '_lr' + str(lr) \
-                + '_dr' + str(decay_rate) \
-                + '_mom' + str(mom) \
-                + '_bs' + str(args.batch_size) \
-                + '_tf' + str(train_frac) \
-                # + '_steps' + str(steps)
-        results_dir = os.path.join(out_dir,
-                                    'results',
-                                    args.result_dir,
-                                    run_name + '_' + str(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")))
-        save_args(args, results_dir)
+            run_name = args.name \
+                    + '_lr' + str(lr) \
+                    + '_mom' + str(mom) \
+                    + '_bs' + str(args.batch_size) \
+                    + '_ep' + str(args.epochs)
+                    # + '_dr' + str(decay_rate) \
+                    # + '_steps' + str(steps)
+            if train_frac is not None:
+                run_name += '_tf' + str(train_frac) \
 
-        for trial_iter in range(args.trials):
-            run_iter_name = run_name + '_' + str(trial_iter)
-            log_path = os.path.join(out_dir, 'tensorboard', args.result_dir, run_iter_name)
-            checkpoint_path = os.path.join(out_dir, 'checkpoints', args.result_dir, run_iter_name)
-            result_path = os.path.join(results_dir, str(trial_iter))
-            # vis_path = os.path.join(out_dir, 'vis', args.result_dir, run_iter_name)
+            results_dir = os.path.join(out_dir,
+                                        'results',
+                                        args.result_dir,
+                                        run_name + '_' + str(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")))
+            save_args(args, results_dir)
 
-            # loss_name_fn = get_loss(params)
-            # net = construct_net(params)
-            # model = nets[args.model](args) # TODO: move args out
-            model = construct_model(nets[args.model], dataset.in_size, dataset.out_size, args) # TODO: move args out
-            optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=mom)
-            losses, accuracies = optimize_torch(dataset, model, optimizer, args.epochs, log_path, checkpoint_path, result_path, args.test)
+            for trial_iter in range(args.trials):
+                log_path = os.path.join(out_dir, 'tensorboard', args.result_dir, run_name, str(trial_iter))
+                checkpoint_path = os.path.join(out_dir, 'checkpoints', args.result_dir, run_name, str(trial_iter))
+                result_path = os.path.join(results_dir, str(trial_iter))
+                # vis_path = os.path.join(out_dir, 'vis', args.result_dir, run_iter_name)
+
+                model = construct_model(nets[args.model], dataset.in_size, dataset.out_size, args) # TODO: move args out
+                optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=mom)
+                losses, accuracies = optimize_torch(dataset, model, optimizer, args.epochs, log_path, checkpoint_path, result_path, args.test)
 
 
 
@@ -342,23 +276,22 @@ mlp_parser = subparsers.add_parser('MLP')
 mlp_parser.set_defaults(task=mlp)
 vae_parser = subparsers.add_parser('VAE')
 vae_parser.set_defaults(task=vae)
+# possible other main commands: sample() for the sample complexity case, vae(), etc.
 
 # MLP models
-fats = []
+model_options = []
 nets = {}
 for model in ArghModel.__subclasses__():
     # change the names so argh can create parsers
     model.init.__name__ = model.__name__
-    fats.append(model.init)
+    model_options.append(model.init)
     print(model.__name__, signature(model.init).parameters)
     nets[model.__name__] = model
-print('nets: ', nets)
-argh.add_commands(mlp_parser, fats, namespace='model', namespace_kwargs={'dest': 'model'})
+argh.add_commands(mlp_parser, model_options, namespace='model', namespace_kwargs={'dest': 'model'})
 for model in ArghModel.__subclasses__():
     # change names back
     model.init.__name__ = 'init'
 
 
 args = parser.parse_args()
-print('args: ', args)
 args.task(args)
