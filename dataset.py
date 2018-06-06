05d236d16230699415945a42d7ac8582a69df704
Albert Gu <albertgu@stanford.edu>
2018-06-05 21:45:31 -0700

    remove attention dataset classes

--- dataset.py
+++ dataset.py
@@ -14,73 +14,7 @@ from torch_utils import *
 from torchtext import data
 from torchvision import datasets, transforms
 
-# TODO put this in separate file for attention codepath
-class MyIterator(data.Iterator):
-    def create_batches(self):
-        if self.train:
-            def pool(d, random_shuffler):
-                for p in data.batch(d, self.batch_size * 100):
-                    p_batch = data.batch(
-                        sorted(p, key=self.sort_key),
-                        self.batch_size, self.batch_size_fn)
-                    for b in random_shuffler(list(p_batch)):
-                        yield b
-            self.batches = pool(self.data(), self.random_shuffler)
-
-        else:
-            self.batches = []
-            for b in data.batch(self.data(), self.batch_size,
-                                          self.batch_size_fn):
-                self.batches.append(sorted(b, key=self.sort_key))
-
-def rebatch(pad_idx, batch):
-    "Fix order in torchtext to match ours"
-    src, trg = batch.src.transpose(0, 1), batch.trg.transpose(0, 1)
-    return Batch(src, trg, pad_idx)
-
-def data_gen(V, batch, nbatches):
-    "Generate random data for a src-tgt copy task."
-    for i in range(nbatches):
-        data = torch.from_numpy(np.random.randint(1, V, size=(batch, 10)))
-        data[:, 0] = 1
-        src = Variable(data, requires_grad=False).cuda()
-        tgt = Variable(data, requires_grad=False).cuda()
-        yield Batch(src, tgt, 0)
-
-class Batch:
-    "Object for holding a batch of data with mask during training."
-    def __init__(self, src, trg=None, pad=0):
-        self.src = src
-        self.src_mask = (src != pad).unsqueeze(-2)
-        if trg is not None:
-            self.trg = trg[:, :-1]
-            self.trg_y = trg[:, 1:]
-            self.trg_mask = \
-                self.make_std_mask(self.trg, pad)
-            self.ntokens = (self.trg_y != pad).data.sum()
-
-    @staticmethod
-    def make_std_mask(tgt, pad):
-        "Create a mask to hide padding and future words."
-        tgt_mask = (tgt != pad).unsqueeze(-2)
-        tgt_mask = tgt_mask & Variable(
-            subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
-        return tgt_mask
-
-global max_src_in_batch, max_tgt_in_batch
-def batch_size_fn(new, count, sofar):
-    "Keep augmenting batch and calculate total number of tokens + padding."
-    global max_src_in_batch, max_tgt_in_batch
-    if count == 1:
-        max_src_in_batch = 0
-        max_tgt_in_batch = 0
-    max_src_in_batch = max(max_src_in_batch,  len(new.src))
-    max_tgt_in_batch = max(max_tgt_in_batch,  len(new.trg) + 2)
-    src_elements = count * max_src_in_batch
-    tgt_elements = count * max_tgt_in_batch
-    return max(src_elements, tgt_elements)
-
-
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def get_dataset(dataset_name):
     """
@@ -193,7 +127,6 @@ def split_train_val(train_X, train_Y, val_size, train_fraction=None, val_fractio
 
 
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 def create_data_loaders(dataset_name, transform, train_fraction, val_fraction, batch_size):
     if device.type == 'cuda':
         loader_args = {'num_workers': 16, 'pin_memory': True}
