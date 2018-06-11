12008942d6b91063554a5fd6b98a9672f0dfefae
Albert Gu <albertgu@stanford.edu>
2018-06-10 20:31:21 -0700

    move set_mult_fns out of structured layers file

--- pytorch/mlp/old_utils/torch_krylov.py
+++ pytorch/mlp/old_utils/torch_krylov.py
@@ -42,3 +42,55 @@ def tridiag_transpose_mult_fn(subdiag_f, diag, supdiag, x):
     diag_result = x*diag
 
     return sup_result + sub_result + diag_result
+
+
+
+# Assumes Stein displacement.
+def set_mult_fns(self, params):
+    # assert params.disp_type == 'stein'
+    if params.class_type in ['toeplitz_like', 'toep_corner', 'toep_nocorn']:
+        fn_A = functools.partial(Z_mult_fn, 1)
+        fn_B_T = functools.partial(Z_mult_fn, -1)
+    # TODO: operators for hankel and vandermonde have not been checked for transpose consistency
+    elif params.class_type == 'hankel_like':
+        fn_A = functools.partial(Z_transpose_mult_fn, 1)
+        fn_B_T = functools.partial(Z_mult_fn, 0)
+    elif params.class_type == 'vandermonde_like':
+        v = Parameter(torch.Tensor(params.layer_size))
+        torch.nn.init.normal_(v,std=params.init_stddev)
+        self.v = v
+        fn_A = functools.partial(diag_mult_fn, self.v)
+        fn_B_T = functools.partial(Z_transpose_mult_fn, 0)
+    elif params.class_type == 'circulant_sparsity':
+        self.subdiag_f_A = Parameter(torch.Tensor(params.layer_size))
+        self.subdiag_f_B = Parameter(torch.Tensor(params.layer_size))
+        torch.nn.init.normal_(self.subdiag_f_A,std=params.init_stddev)
+        torch.nn.init.normal_(self.subdiag_f_B,std=params.init_stddev)
+
+        fn_A = functools.partial(circ_mult_fn, self.subdiag_f_A)
+        fn_B_T = functools.partial(circ_mult_fn, self.subdiag_f_B)
+
+    elif params.class_type == 'tridiagonal_corner':
+        self.subdiag_f_A = Parameter(torch.Tensor(params.layer_size))
+        self.subdiag_f_B = Parameter(torch.Tensor(params.layer_size))
+        self.diag_A = Parameter(torch.Tensor(params.layer_size))
+        self.diag_B = Parameter(torch.Tensor(params.layer_size))
+        self.supdiag_A = Parameter(torch.Tensor(params.layer_size-1))
+        self.supdiag_B = Parameter(torch.Tensor(params.layer_size-1))
+
+        torch.nn.init.normal_(self.subdiag_f_A,std=params.init_stddev)
+        torch.nn.init.normal_(self.subdiag_f_B,std=params.init_stddev)
+        torch.nn.init.normal_(self.diag_A,std=params.init_stddev)
+        torch.nn.init.normal_(self.diag_B,std=params.init_stddev)
+        torch.nn.init.normal_(self.supdiag_A,std=params.init_stddev)
+        torch.nn.init.normal_(self.supdiag_B,std=params.init_stddev)
+
+        fn_A = functools.partial(tridiag_mult_fn, self.subdiag_f_A, self.diag_A, self.supdiag_A)
+        fn_B_T = functools.partial(tridiag_mult_fn, self.subdiag_f_B, self.diag_B, self.supdiag_B)
+
+    else:
+        print(('Not supported: ', params.class_type))
+        assert 0
+    return fn_A, fn_B_T
+
+
