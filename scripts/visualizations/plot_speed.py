e3d94cda9e6027eab4ddf02d3dcc9019ffd5910b
thomasat <thomasat@stanford.edu>
2018-07-06 18:44:41 -0700

    fix paths in plot_speed

--- scripts/visualizations/plot_speed.py
+++ scripts/visualizations/plot_speed.py
@@ -2,9 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from timeit import default_timer as timer
 
-import toeplitz_cpu as toep
-import triXXF as subd
+import sys
+sys.path.insert(0,'../../pytorch/')
 
+import structure.toeplitz_cpu as toep
+import structure.triXXF as subd
 
 # sizes = [1<<9, 1<<10, 1<<11, 1<<12, 1<<13, 1<<14, 1<<15]
 exps = np.arange(7, 16)
