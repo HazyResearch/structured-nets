066786fb0a8f20941e90e57cc01e5643d8d6d37a
Tri Dao <tridpq@gmail.com>
2018-08-16 17:21:09 -0700

    Delete old comments in complex_utils.py

--- pytorch/structure/complex_utils.py
+++ pytorch/structure/complex_utils.py
@@ -1,13 +1,9 @@
 ''' Utility functions for handling complex tensors: conjugate and complex_mult.
 Pytorch (as of 0.4.0) does not support complex tensors, so we store them as
 float tensors where the last dimension is 2 (real and imaginary parts).
-Cupy does support complex arrays, so we can cast Pytorch tensor to Cupy array
-and use the multiplication function there. If Cupy isn't available, we use our
-own complex multiplication implemented in Pytorch, which is about 6x slower.
 '''
 
 import torch
-from torch.utils.dlpack import to_dlpack, from_dlpack
 
 
 def conjugate(X):
